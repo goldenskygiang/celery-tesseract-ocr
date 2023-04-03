7edbee383b079f341d256a5c2d3df9c12baa956b
Vu Truong Giang <giang.vt@teko.vn>
2023-04-03 16:57:40 +0700

    sqlite by default

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
-#DATABASE_URL = 'sqlite:///./celery_ocr.db'
-DATABASE_URL = "postgresql://postgres:postgres@localhost/celery-ocr"
+DATABASE_URL = 'sqlite:///./celery_ocr.db'
+#DATABASE_URL = "postgresql://postgres:postgres@localhost/celery-ocr"
 
 CELERY_BACKEND = f'db+{DATABASE_URL}'
 CELERY_BROKER = 'redis://default:redispw@localhost:32768'
